b2f7297bfb53832c200ad78f04f7b7b95cb56c71
Alex A. Mihaylov <minimumlaw@rambler.ru>
2018-02-13 11:48:55 +0300

    VFxxx - cosmetic fix. NOT WORKING

--- build_vf.sh
+++ build_vf.sh
@@ -5,14 +5,14 @@ export CROSS_COMPILE=armv7a-hardfloat-linux-gnueabi-
 
 if [ -z ${1} ]; then
     if [ -f .config ]; then
-	make -j5
+	make -j3
     else
 	make clean distclean
 	make colibri_vf_defconfig
-	make -j5
+	make -j3
     fi
     if [ -f ./u-boot-nand.imx ]; then
-	echo Update TFTP u-boot.imx
+	echo Update TFTP u-boot-nand.imx
 	cp -f ./u-boot-nand.imx /cimc/exporttftp/
 	md5sum ./u-boot-nand.imx /cimc/exporttftp/u-boot-nand.imx
     fi
