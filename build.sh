c7c53426f9dc29667c74e042acf62f2e1f5b9042
Alex A. Mihaylov <minimumlaw@rambler.ru>
2018-11-18 12:24:29 +0300

    Apply speakingcity defconfig (with disabled video)

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export CROSS_COMPILE=arm-linux-gnueabihf-
 if [ -f .config ]; then
 	make $*
 else
-	make mx6ul_var_dart_mmc_defconfig
+	make mx6ul_var_dart_speakingcity_defconfig
 #	make mx6ul_var_dart_mmc_SECURE_BOOT_defconfig
 	make $*
 fi
