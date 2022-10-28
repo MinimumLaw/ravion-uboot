9fc666a7f7cb2c8b085e1d1c17602b9824011d6b
Alex A. Mihaylov <minimumlaw@rambler.ru>
2022-10-28 14:28:16 +0300

    Ravion-V2: OTG port PEN signal active high defconfig change
    - try to imply DISTO_BOOTCOMMAND in defconfig
    - remove ALL ravion bootcmd, try set distro_ backward compatible

--- include/configs/ravion_emmcbootonly_imx6.h
+++ include/configs/ravion_emmcbootonly_imx6.h
@@ -33,9 +33,16 @@
 SDRAM START - 0x1000 0000
 */
 
-/* don't like mx6_common.h default */
-#undef  CONFIG_SYS_LOAD_ADDR
-
+/*****************************************************************************
+ * Fixup memory layout
+ * DISTRO_XXX requred for :
+ *  = fdt_addr		- absent, use $(fdtcontroladdr)
+ *  = fdt_addr_r	- defined
+ *  = ramdisk_addr_r	- defined
+ *  = kernel_addr_r	- defined
+ *  = pxefile_addr_r	- defined
+ *  = scriptaddr	- definded
+ */
 #define MEM_LAYOUT_ENV_SETTINGS \
 "loadaddr_low_r=0x10400000\0" \
  "kernel_addr_r=0x11400000\0" \
@@ -43,19 +50,28 @@ SDRAM START - 0x1000 0000
    "initrd_high=0x1f3fffff\0" \
     "fdt_addr_r=0x1f400000\0" \
       "fdt_high=0x1f7fffff\0" \
- "script_addr_r=0x1f800000\0" \
+"pxefile_addr_r=0x1f800000\0" \
+    "scriptaddr=0x1f800000\0" \
+
+/* don't like mx6_common.h default */
+#undef  CONFIG_SYS_LOAD_ADDR
+#define CONFIG_SYS_LOAD_ADDR		0x10400000 /* $load_addr_low_r */
 
 #ifdef CONFIG_SPL		/* SPL BUILD */
+/* #warning SPL Build */
 #include "imx6_spl.h"
 /* SPL Basic params */
-#define CONFIG_SYS_LOAD_ADDR		0x11400000  /* kernel_addr_r */
 #define CONFIG_MXC_UART_BASE		UART1_BASE  /* debug console */
 /* SPL MMC Configuration */
 #define CONFIG_SYS_FSL_ESDHC_ADDR	USDHC3_BASE_ADDR
-#else				/* UBOOT BULD */
-/* U-Bool default load address (16Mb block) */
-#define CONFIG_SYS_LOAD_ADDR		0x10400000 /* SDRAM_START + 4Mb */
 #endif
+
+#define BOOT_TARGET_DEVICES(func) \
+        func(USB, usb, 0) \
+        func(MMC, mmc, 1) \
+        func(MMC, mmc, 0) \
+        func(SATA, sata, 0)
+#include <config_distro_bootcmd.h>
 
 #define CONFIG_CMDLINE_TAG
 #define CONFIG_SETUP_MEMORY_TAGS
@@ -68,68 +84,21 @@ SDRAM START - 0x1000 0000
 #define CONFIG_NETMASK			255.255.255.0
 #define CONFIG_SERVERIP			192.168.5.254
 
-#define BLKDEV_BOOTCMD \
-	"efiboot=bootefi bootmgr\0" \
-	"_blkdevboot=" \
-	"sysboot ${blkname} ${blkdev} any " \
-	"${script_addr_r} syslinux.conf\0" \
-	"_scriptboot=" \
-	"load ${blkname} ${blkdev} ${script_addr_r} " \
-	"${boot_script_file} && source ${script_addr_r}\0"
-
-/*
- * USB storage.
- * Boot script & syslinux.conf from SECOND partition (slow).
- * 1-st partition may contents recovery mode falcon files,
- * u-boot-dtb.img
- */
-#define USB_BOOTCMD \
-	"usbboot=" \
-	"setenv blkname usb && " \
-	"setenv blkdev 0:1 && " \
-	"run _scriptboot; run _blkdevboot\0"
-
-/*
- * SD/MMC card removable storage
- */
-#define SD_BOOTCMD \
-	"sdboot=" \
-	"setenv blkname mmc && " \
-	"setenv blkdev 1:1 && " \
-	"run _scriptboot; run _blkdevboot\0"
-
 /*
  * Network/TFTP - only bootscript allowed.
  */
 #define TFTP_BOOTCMD \
+	"bootcmd_tftp0=" \
+	"run tftpboot\0" \
 	"tftpboot=" \
-	"setenv boot_script_file /boot/bscript.img; "\
-	"tftp ${script_addr_r} ${serverip}:${boot_script_file} && " \
-	"source ${script_addr_r} || env default boot_script_file\0"
-
-/*
- * eMMC storage.
- * Boot syslinux.conf & script from first and second (compatibe) partition(s).
- */
-#define EMMC_BOOTCMD \
-	"emmcboot=" \
-	"setenv blkname mmc && " \
-	"setenv blkdev 0:1 && " \
-	"run _scriptboot; run _blkdevboot\0" \
-	"emmcboot2=" \
-	"setenv blkname mmc && " \
-	"setenv blkdev 0:2 && " \
-	"run _scriptboot; run _blkdevboot\0"
-
-/*
- * SATA storage.
- * Boot syslinux.conf & script from FIRST partition (slow).
- */
-#define SATA_BOOTCMD \
-	"sataboot="\
-	"setenv blkname sata; " \
-	"setenv blkdev 0:1; " \
-	"run _scriptboot; run _blkdevboot\0"
+	    "for prefix in ${boot_prefixes}; do " \
+		"for script in ${boot_scripts}; do " \
+		    "if tftp ${scriptaddr} ${serverip}:${prefix}${script}; then " \
+			"echo Found ${prefix}${script} on tftpserver; " \
+			"source ${scriptaddr}; " \
+		    "fi; " \
+		"done;" \
+	    "done\0"
 
 #ifdef CONFIG_MX6Q
 #define VARIANT	"variant=qp\0"
@@ -137,33 +106,30 @@ SDRAM START - 0x1000 0000
 #define VARIANT	"variant=dl\0"
 #endif
 
+/*****************************************************************************
+ * Required for back compatible
+ ****************************************************************************/
+#define DISTRO_FIXUP \
+	"boot_syslinux_conf=syslinux.conf\0" \
+	"board=kitsbimx6\0" \
+	"vendor_required=" \
+	    "env info -d && saveenv; " \
+	    "setenv boot_scripts bscript.img ${boot_scripts}; " \
+	    "setenv boot_targets ${boot_targets} tftp0; " \
+	    "setenv nfs_path /cimc/root/colibri-imx6; " \
+	    "setenv fdtfile i${soc}${variant}-${vendor}-${board}.dtb\0" \
+	"bootcmd=run vendor_required; run distro_bootcmd\0"
+
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"__INF0__=Ravion-V2 I.MX6 CPU Module BSP package\0" \
 	"__INF1__=Created: Alex A. Mihaylov AKA MinimumLaw, MinimumLaw@Rambler.Ru\0" \
 	"__INF2__=Request: Radioavionica Corp, Saint-Petersburg, Russia, 2022\0" \
 	"__INF3__=License: GPL v2 and above, https://github.com/MinimumLaw\0" \
-	"board=kitsbimx6\0" \
-	"bootcmd=echo BOOT DONE\0" \
-	"distro_bootcmd=" \
-	    "run efiboot; " \
-	    "run usbboot; " \
-	    "run sdboot; " \
-	    "run emmcboot; " \
-	    "run emmcboot2; " \
-	    "run sataboot; " \
-	    "ums 0 mmc 0\0" \
-	"server_path=/cimc/root/colibri-imx6\0" \
-	"boot_script_file=bscript.img\0" \
-	"board_name=\0" \
-	VARIANT \
 	MEM_LAYOUT_ENV_SETTINGS \
-	BLKDEV_BOOTCMD \
-	USB_BOOTCMD \
-	SD_BOOTCMD \
+	VARIANT \
+	BOOTENV \
 	TFTP_BOOTCMD \
-	EMMC_BOOTCMD \
-	SATA_BOOTCMD \
-	"splashpos=m,m\0"
+	DISTRO_FIXUP
 
 /* Miscellaneous configurable options */
 #undef CONFIG_SYS_CBSIZE
