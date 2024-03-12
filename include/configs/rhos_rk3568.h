dce4c6f8bc4ca9bbe3270a6cd338abdaeeb5225e
Alex A. Mihaylov <minimumlaw@rambler.ru>
2024-03-12 09:50:03 +0300

    RHOS: RK3568: bringup and improove
    	[+] - gmac use internal clock source
    	[!] - use new style env

--- include/configs/rhos_rk3568.h
+++ include/configs/rhos_rk3568.h
@@ -6,25 +6,8 @@
 #ifndef __RHOS_RK3568_H
 #define __RHOS_RK3568_H
 
+#define ROCKCHIP_DEVICE_SETTINGS
+
 #include "rk3568_common.h"
-
-#define RAVBOOT \
-	"fdt_file=rk3568-rhos-kitsbimx8.dtb\0" \
-	"ravboot=tftpboot ${loadaddr} ${serverip}:/boot/bscript.img; source ${loadaddr}\0" \
-	"bootcmd=run ravboot\0"
-
-#undef CFG_EXTRA_ENV_SETTINGS
-#define CFG_EXTRA_ENV_SETTINGS \
-	"__INF1__=RHOS RK3568 CPU Module BSP package\0" \
-	"__INF2__=Created: Alex A. Mihaylov AKA MinimumLaw, MinimumLaw@Rambler.Ru\0" \
-	"__INF3__=Request: Radioavionica Corp, Saint-Petersburg, Russia, 2024\0" \
-	"__INF4__=License: GPL v2 and above, https://github.com/MinimumLaw\0" \
-	ENV_MEM_LAYOUT_SETTINGS \
-	RAVBOOT \
-	"ipaddr=192.168.5.91\0" \
-	"netmask=255.255.255.0\0" \
-	"serverip=192.168.5.254\0" \
-	"gatewayip=192.168.5.254\0" \
-	"board=kitsbimx8"
 
 #endif
