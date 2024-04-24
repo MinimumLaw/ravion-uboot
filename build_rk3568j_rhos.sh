d76deffd98b04ef87b0d6ca0620184ab3615128e
Alex A. Mihaylov <minimumlaw@rambler.ru>
2024-04-24 18:31:23 +0300

    RHOS: RK3568: sync with linux kernel, try fix env, cfgblock

--- build_rk3568j_rhos.sh
+++ build_rk3568j_rhos.sh
@@ -6,11 +6,12 @@
 #
 # 1. Requre cloned https://github.com/rockchip-linux/rkbin.git into ./../rkbin
 #    Be warned: need to fix debug uart speed to 115200 as defined in this code
-# 2. Fixup BL31 and ROCKCHIP_TPL env bellow, if required
+# 2. Fixup BL31, TEE and ROCKCHIP_TPL env bellow, if required
 # 3. Write u-boot-rockcip.bin with offset 64 (0x40) to SD/eMMC
 #
 ##############################################################################
-export BL31=../rkbin/bin/rk35/rk3568_bl31_ultra_v2.17.elf
+export BL31=../rkbin/bin/rk35/rk3568_bl31_v1.44.elf
+# export TEE=../rkbin/tee.elf
 export ROCKCHIP_TPL=../rkbin/bin/rk35/rk3568_ddr_1560MHz_v1.21.bin
 
 if [ -z $CROSS_COMPILE ]; then
